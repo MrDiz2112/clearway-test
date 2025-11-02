31085e12db40c21c6aad2f79195c8268e64bfe90
lukin <it.v.lukin@yandex.com>
2025-11-02 19:13:23 +0300

    api url as DI token

--- src/app/features/document-viewer/components/img-page/img-page.component.ts
+++ src/app/features/document-viewer/components/img-page/img-page.component.ts
@@ -1,6 +1,7 @@
-import {Component, input} from '@angular/core';
+import {Component, computed, inject, input} from '@angular/core';
 import {ImgPage} from '../../models/img-page';
 import {AnnotationsLayerComponent} from '../annotations-layer/annotations-layer.component';
+import {API_URL} from '../../../../tokens/api-url.token';
 
 @Component({
   selector: 'app-img-page',
@@ -12,4 +13,7 @@ import {AnnotationsLayerComponent} from '../annotations-layer/annotations-layer.
 })
 export class ImgPageComponent {
   imgPage = input.required<ImgPage>();
+  protected readonly apiUrl = inject(API_URL);
+
+  protected imgSrc = computed(() => this.apiUrl + this.imgPage().imageUrl)
 }
--- src/app/features/document-viewer/services/document-api.service.ts
+++ src/app/features/document-viewer/services/document-api.service.ts
@@ -2,15 +2,17 @@ import {inject, Injectable} from '@angular/core';
 import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {ImgDocument} from '../models/img-document';
 import {catchError, delay, throwError} from 'rxjs';
+import {API_URL} from '../../../tokens/api-url.token';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DocumentApiService {
   private http = inject(HttpClient);
+  private apiUrl = inject(API_URL);
 
   getDocumentById(id: string) {
-    return this.http.get<ImgDocument>(`/mocks/${id}.json`).pipe(
+    return this.http.get<ImgDocument>(`${this.apiUrl}${id}.json`).pipe(
       delay(500),
       catchError((error: HttpErrorResponse) => {
         const errorMessage = error.error?.message || error.message || 'Failed to load document';
